c08a004de35b2c0534cc27d8fa102c46e40c7220
Alexandre Da Matta <aledamatta@bol.com.br>
2026-03-15 18:35:03 -0300

    Estruturação do monolith dmlog

---
+++ app/src/test/java/br/com/damatta/dmlog/DmlogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.damatta.dmlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
